f7109663b4be285fe21f94161006e872b6edbfdc
opelr <ryan.a.opel@gmail.com>
2018-12-11 09:21:30 -0800

    Adding migrations

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from app import db, login
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
